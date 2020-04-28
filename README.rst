9686b3eb642c7af8eb82f569aa8a241bda23e1a5
Yannick Koehler <ykoehler@arubanetworks.com>
2020-04-28 10:56:47 -0400

    Added interface parameter

---
+++ README.rst
@@ -0,0 +1,17 @@
+Python FillFDB
+==============
+    
+.. contents:: Topics
+
+Overview
+--------
+
+.. code-block:: shell-session
+
+   # 1) install fillfdb
+   pip install fillfdb
+
+   # 2) next call "codecov" at end of CI build
+   # public repo using Travis, CircleCI or AppVeyor
+   fillfdb -i <iface> -c <count>
+
--- fillfdb/fillfdb.py
+++ fillfdb/fillfdb.py
@@ -4,14 +4,8 @@ import random
 
 SELF_MAC = '02:02:02:02:02:02'    # fill in with your MAC address
 BCAST_MAC = 'ff:ff:ff:ff:ff:ff'
-IFACE = 'enp15s0'
 
-def create_ARP_request_gratuituous():
-    mac = '00:24:' + hex(random.randint(0x10, 0x6f)).lstrip('0x') + ':' + hex(random.randint(0x10, 0x6f)).lstrip('0x') + ':' + hex(random.randint(0x10, 0x6f)).lstrip('0x') + ':' + hex(random.randint(0x10, 0x6f)).lstrip('0x')
-
-    ipaddr = '10.0.0.' + str(random.randint(150,160))
-
-    print("Sending packet with mac " + mac + " and ip " + ipaddr)
+def create_ARP_request_gratuituous(mac, ipaddr):
     arp = ARP(psrc=ipaddr,
               hwsrc=mac,
               pdst=ipaddr)
@@ -19,9 +13,14 @@ def create_ARP_request_gratuituous():
     
 @click.command()
 @click.option("--count", "-c", "count", default=1, help="Number of gratuituous ARP to send.")
-def broadcast(count):
+@click.option("--iface", "-i", "iface", required=True, help="Network Interface to send the packet over.")
+def broadcast(count, iface):
     for i in range(count):
-        sendp(create_ARP_request_gratuituous(), iface=IFACE)
+        mac = '00:24:' + hex(random.randint(0x10, 0x6f)).lstrip('0x') + ':' + hex(random.randint(0x10, 0x6f)).lstrip('0x') + ':' + hex(random.randint(0x10, 0x6f)).lstrip('0x') + ':' + hex(random.randint(0x10, 0x6f)).lstrip('0x')
+        ipaddr = '10.0.0.' + str(random.randint(150,160))
+        print("Sending packet with mac " + mac + " and ip " + ipaddr + "on the Network Interfae " + iface)
+
+        sendp(create_ARP_request_gratuituous(mac, ipaddr), iface=iface)
 
 if __name__ =="__main__":
     broadcast()
